ae14acb9f841e502b2e70fef6f303938c455ff9c
Florian Meier <Florian.Meier@informatik.uni-erlangen.de>
2016-07-14 12:07:26 -0700

    gcov: add support for gcc version >= 6
    commit d02038f972538b93011d78c068f44514fbde0a8c upstream.
    
    Link: http://lkml.kernel.org/r/20160701130914.GA23225@styxhp
    Signed-off-by: Florian Meier <Florian.Meier@informatik.uni-erlangen.de>
    Reviewed-by: Peter Oberparleiter <oberpar@linux.vnet.ibm.com>
    Tested-by: Peter Oberparleiter <oberpar@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/gcov/gcc_4_7.c
+++ kernel/gcov/gcc_4_7.c
@@ -18,10 +18,10 @@
 #include <linux/vmalloc.h>
 #include "gcov.h"
 
-#if __GNUC__ == 5 && __GNUC_MINOR__ >= 1
-#define GCOV_COUNTERS			11
+#if (__GNUC__ > 5) || (__GNUC__ == 5 && __GNUC_MINOR__ >= 1)
+#define GCOV_COUNTERS			10
 #elif __GNUC__ == 4 && __GNUC_MINOR__ >= 9
-#define GCOV_COUNTERS			11
+#define GCOV_COUNTERS			9
 #else
 #define GCOV_COUNTERS			8
 #endif
