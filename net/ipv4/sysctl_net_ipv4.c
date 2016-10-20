a51e5d371f7e3815474f99562d323f2d7cf9b94d
Eric Dumazet <edumazet@google.com>
2016-10-20 10:26:48 -0700

    ipv4: disable BH in set_ping_group_range()
    [ Upstream commit a681574c99be23e4d20b769bf0e543239c364af5 ]
    
    In commit 4ee3bd4a8c746 ("ipv4: disable BH when changing ip local port
    range") Cong added BH protection in set_local_port_range() but missed
    that same fix was needed in set_ping_group_range()
    
    Fixes: b8f1a55639e6 ("udp: Add function to make source port for UDP tunnels")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Eric Salo <salo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -111,7 +111,6 @@ static void set_ping_group_range(struct ctl_table *table, kgid_t low, kgid_t hig
 	struct net *net =
 		container_of(table->data, struct net, ipv4.ping_group_range.range);
 	write_seqlock_bh(&net->ipv4.ping_group_range.lock);
-
 	data[0] = low;
 	data[1] = high;
 	write_sequnlock_bh(&net->ipv4.ping_group_range.lock);
