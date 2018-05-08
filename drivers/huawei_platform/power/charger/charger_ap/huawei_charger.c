5b06569a4b01d5816ad59d2bae079d8b5627900c
engstk <eng.stk@sapo.pt>
2018-05-08 20:23:25 +0100

    drivers: misc: implement usb fast charge mode
    Enables force charging up to 900mA in usb mode
    
    SYSFS:
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/huawei_platform/power/charger/charger_ap/huawei_charger.c
+++ drivers/huawei_platform/power/charger/charger_ap/huawei_charger.c
@@ -69,6 +69,9 @@
 
 extern struct ycable_info *ycable;
 #endif
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
 
 /*adaptor test result*/
 struct adaptor_test_attr adptor_test_tbl[] = {
@@ -1430,6 +1433,13 @@ static void charge_select_charging_current(struct charge_device_info *di)
 	case CHARGER_TYPE_USB:
 		di->input_current = di->core_data->iin_usb;
 		di->charge_current = di->core_data->ichg_usb;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge > 0)
+		{
+			di->input_current = CHARGE_CURRENT_0800_MA;
+			di->charge_current = CHARGE_CURRENT_0800_MA;
+		}
+#endif
 		break;
 	case CHARGER_TYPE_NON_STANDARD:
 		di->input_current = di->core_data->iin_nonstd;
