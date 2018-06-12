bbbafc41db3f8c243ad72a1ec20f36d58fe01d34
JBolho <hugo.ss.android@gmail.com>
2018-06-13 00:10:47 +0100

    Update ui_prints on Anykernel Script

--- out/anykernel.sh
+++ out/anykernel.sh
@@ -1,10 +1,10 @@
 # AnyKernel2 Ramdisk Mod Script
-# osm0sis @ xda-developers
+# osm0sis @ XDA
 
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=Proto8 Kernel by JBolho and nutcasev1.5 @ XDA
+kernel.string=Proto8 Kernel
 do.devicecheck=0
 do.modules=0
 do.cleanup=1
@@ -17,17 +17,17 @@ device.name2=
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-ui_print " ";
-ui_print " ";
 ui_print "     ____  ____  ____  __________   ";
 ui_print "    / __ \/ __ \/ __ \/_  __/ __ \  ";
 ui_print "   / /_/ / /_/ / / / / / / / / / /  ";
 ui_print "  / ____/ _, _/ /_/ / / / / /_/ /   ";
 ui_print " /_/   /_/ |_|\____/ /_/  \____/    ";
-ui_print " ";
-ui_print " ";
-ui_print " ";
-
+ui_print "                                    ";
+ui_print "     by JBolho and nutcasev1.5      ";
+ui_print "                                    ";
+ui_print "     beta testing --- Hugoclaw      ";
+ui_print "                                    ";
+ui_print "     Honor 9 and P10 certified      ";
 
 # shell variables
 block=/dev/block/bootdevice/by-name/kernel;
@@ -40,7 +40,7 @@ mv /tmp/anykernel/kernel-Image.gz /tmp/anykernel/Image.gz;
 
 ## AnyKernel kernel install
 
-ui_print "Proto8 Kernel is being installed";
+ui_print "Proto8 Kernel is being infused";
 
 split_boot;
 
