87cb356e3745cf910d45d8e4bf45b5d50f0efcf6
nutcasev15 <tab.andsupp@gmail.com>
2018-06-12 12:57:37 +0100

    INIT: Modified version.c File

--- init/version.c
+++ init/version.c
@@ -44,10 +44,9 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	"Linux version " UTS_RELEASE " (" "REDACTED" ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" "REDACTED" ")"
 	" (" LINUX_COMPILER ") %s\n";
