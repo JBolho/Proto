f42834837644e0899674fb50a81cb19a754324ec
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-21 18:43:51 +0100

    CPU: Silence Log Spam When a CPU is Brought Up
    Notes:
    
    This commit originally also modifies kernel/cpu.c but said modifications
    have already been implemented by commit db1f9e1f587c983c1dc45ce95594241836c89d8b
    on this kernel.

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -244,7 +244,7 @@ asmlinkage void secondary_start_kernel(void)
 	 * the CPU migration code to notice that the CPU is online
 	 * before we continue.
 	 */
-	pr_info("CPU%u: Booted secondary processor [%08x]\n",
+	pr_debug("CPU%u: Booted secondary processor [%08x]\n",
 					 cpu, read_cpuid_id());
 	update_cpu_boot_status(CPU_BOOT_SUCCESS);
 	/* Make sure the status update is visible before we complete */
