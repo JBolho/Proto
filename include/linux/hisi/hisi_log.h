79e1b331b3e66f28628de118772315176f2ce39b
JBolho <hugo.ss.android@gmail.com>
2018-08-31 00:29:47 +0100

    DRIVERS: Port HiSi JPEG Processing Engine

---
+++ include/linux/hisi/hisi_log.h
@@ -0,0 +1,27 @@
+/*
+ * hisi_log.h
+ *
+ * Copyright (c) 2001-2021, Huawei Tech. Co., Ltd. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __HISI_LOG_H__
+#define __HISI_LOG_H__
+
+#undef  pr_fmt
+#define pr_fmt(fmt) "["HISI_LOG_TAG"]:" fmt
+
+#define MNTN_DUMP_TAG "mntn_dump"
+
+
+#endif /* end of hisi_log.h */
